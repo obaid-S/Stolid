61d6d5a3285449aa6d4d27118c5db3528728aeb0
obaid-S <105257992+obaid-S@users.noreply.github.com>
2023-10-20 18:07:04 -0700

    bug fixes
    bounes off blocks on jump, doesnt get stuck to blocks, when jumping doesnt phase through, hitboxes less random

--- Stolid/Assets/Scripts/Extensions.cs
+++ Stolid/Assets/Scripts/Extensions.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public static class Extensions
 {
-    public static bool Raycast(this Rigidbody rigidbody, Vector2 directioon)
+    private static LayerMask Ground = LayerMask.GetMask("Ground");
+
+    public static bool Raycast(this BoxCollider2D col, Vector2 direction)
     {
+        RaycastHit2D hit = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, direction, 0.1f, Ground);
+
+        return hit.collider != null;//if hit something return true
+    }
+
+    public static bool DotTest(this Transform transform, Transform other, Vector2 testdirection)
+    {
+        Vector2 direction = other.position-transform.position;
+        return Vector2.Dot(direction.normalized, testdirection) > 0.3f;
 
     }
-}
+} 
--- Stolid/Assets/Scripts/Movement.cs
+++ Stolid/Assets/Scripts/Movement.cs
@@ -3,30 +3,85 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private new Rigidbody2D rigidbody; 
+    
+    private new Rigidbody2D rigidbody;
+    private BoxCollider2D col;
 
     private Vector2 velocity;
     private float inputAxis;
 
     public float moveSpeed = 15f;
-    public float jumpHeight= 5f;
 
+    public float jumpForce = 5f;
     public bool grounded {get; private set;}
     public bool jumping {get; private set;}
+    public float gravity = 10f;
+
+    
 
     private void Awake(){
         rigidbody = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
     }
     
     private void Update(){
+        grounded= col.Raycast(Vector2.down);
         HorizontalMovement();
+        if (grounded)
+        {
+            VerticalMovement();
+        }
+        ApplyGravity();
+        
     }
 
     private void HorizontalMovement(){
         inputAxis= Input.GetAxis("Horizontal");
-    
-        
+
         velocity.x=inputAxis*moveSpeed;
+        if (velocity.x > 0f)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (velocity.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        if (col.Raycast(Vector2.right * velocity.x))
+        {
+            velocity.x = 0f;
+        }
+    }
+
+    private void VerticalMovement()
+    {
+        velocity.y= Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!col.Raycast(Vector2.up))
+            {
+                velocity.y = jumpForce;
+                jumping = true;
+            }
+            
+        }
+
+        
+    }
+
+    private void ApplyGravity()
+    {
+        //if velocity is negative OR if the jump button isnt being held, second part is to make jump last logner if held
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+
+        float multiplier = falling ? 2f : 1f;
+
+        velocity.y -= gravity * Time.deltaTime * multiplier;
+        
+
     }
 
     private void FixedUpdate(){
@@ -40,4 +95,15 @@ public class Movement : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            if (col.Raycast(Vector2.up))
+            {
+                velocity.y = 0f;
+            }
+            
+        } 
+    }
 }
